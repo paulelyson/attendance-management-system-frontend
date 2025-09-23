6c8d7dba7ea755d8bcd90ec55cd7a54d6757e311
paulelyson <paulelyson.villaceran@gmail.com>
2025-09-23 21:05:51 +0800

    added attendance module and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+ path: '',
+ loadChildren: ()=> import('./modules/attendance/attendance-module').then(m => m.AttendanceModule)
+}];
