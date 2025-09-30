feb998f43354a7c004ae1fb83d952f68273ddeac
paulelyson <paulelyson.villaceran@gmail.com>
2025-09-30 19:51:08 +0800

    added shared input

---
+++ src/app/modules/shared/input/input.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-input',
+  imports: [CommonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() suffix_icon: string = '';
+  // accessor
+  value: string = '';
+  disabled: boolean = false;
+  public changed = (_: any) => {};
+  public touched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onSelectChange(_val: any) {
+    this.changed(this.value);
+  }
+}
