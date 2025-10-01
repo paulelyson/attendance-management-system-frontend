894b518b1c581bfc1e0683113810fb4586f66be0
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-01 16:48:44 +0800

    initial commit on sidenav filter

---
+++ src/app/modules/shared/side-filter/side-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InputComponent } from '../input/input.component';
+
+@Component({
+  selector: 'app-side-filter',
+  imports: [InputComponent],
+  templateUrl: './side-filter.component.html',
+  styleUrl: './side-filter.component.css'
+})
+export class SideFilterComponent {
+
+}
