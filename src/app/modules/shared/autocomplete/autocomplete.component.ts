da5b069bdb4b824c0d29d177ed3422f2910455bb
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-01 12:48:38 +0800

    initial commit on shared autcomplete

---
+++ src/app/modules/shared/autocomplete/autocomplete.component.ts
@@ -0,0 +1,41 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { map, Observable, startWith } from 'rxjs';
+
+@Component({
+  selector: 'app-autocomplete',
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.css',
+})
+export class AutocompleteComponent implements OnInit {
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+
+  constructor() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+  }
+
+  ngOnInit() {}
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) => option.toLowerCase().includes(filterValue));
+  }
+}
