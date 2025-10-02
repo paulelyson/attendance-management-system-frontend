e1903c5ead98212306368d051ea2f9b5a278d419
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-02 13:49:31 +0800

    updates on title section

--- src/app/modules/shared/title-section/title-section.component.ts
+++ src/app/modules/shared/title-section/title-section.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-title-section',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './title-section.component.html',
   styleUrl: './title-section.component.css'
 })
