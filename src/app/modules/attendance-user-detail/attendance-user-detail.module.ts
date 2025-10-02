39d73d48ff0657a4db25189b380e1c9cc99fcacb
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-02 16:16:25 +0800

    updates on attendance user detail

--- src/app/modules/attendance-user-detail/attendance-user-detail.module.ts
+++ src/app/modules/attendance-user-detail/attendance-user-detail.module.ts
@@ -2,12 +2,12 @@ import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AttendanceUserDetailRoutingModule } from './attendance-user-detail-routing.module';
-import { TableComponent } from '../shared/table/table.component';
 import { AttendanceUserDetailComponent } from './attendance-user-detail/attendance-user-detail.component';
 import { MatTableModule } from '@angular/material/table';
 import { AttendanceUserDetailTableComponent } from './attendance-user-detail-table/attendance-user-detail-table.component';
+import { InfoCardComponent } from '../shared/info-card/info-card.component';
 @NgModule({
   declarations: [AttendanceUserDetailComponent, AttendanceUserDetailTableComponent],
-  imports: [CommonModule, AttendanceUserDetailRoutingModule, MatTableModule],
+  imports: [CommonModule, AttendanceUserDetailRoutingModule, MatTableModule, InfoCardComponent],
 })
 export class AttendanceUserDetailModule {}
