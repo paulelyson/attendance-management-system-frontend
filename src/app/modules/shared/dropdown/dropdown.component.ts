9cc3dc9cc8d9437f56986ff9b5f956c9dc9f9fe5
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-02 16:14:23 +0800

    addedcontrol value accessor on dropdown

--- src/app/modules/shared/dropdown/dropdown.component.ts
+++ src/app/modules/shared/dropdown/dropdown.component.ts
@@ -1,18 +1,45 @@
-import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-dropdown',
   imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DropdownComponent),
+      multi: true,
+    },
+  ],
 })
-export class DropdownComponent {
-  @Input() label: string = ''
-  @Input() options = [];
+export class DropdownComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() options: any[] = [];
+  // accessor
+  value: string = '';
+  disabled: boolean = false;
+  public changed = (_: any) => {};
+  public touched = () => {};
 
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onSelectionChange(event: MatSelectChange): void {
+    this.changed(event.value);
+  }
 }
