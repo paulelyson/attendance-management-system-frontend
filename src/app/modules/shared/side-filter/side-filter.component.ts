d2afbef9d42e1a59a83e172b10b4529d6718c77a
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-02 13:49:00 +0800

    update on side filter

--- src/app/modules/shared/side-filter/side-filter.component.ts
+++ src/app/modules/shared/side-filter/side-filter.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../input/input.component';
+import { DatepickerComponent } from '../datepicker/datepicker.component';
+import { AttendanceStatus } from '../../../models/UserDailyAttendance';
+import { DropdownComponent } from '../dropdown/dropdown.component';
 
 @Component({
   selector: 'app-side-filter',
-  imports: [InputComponent],
+  imports: [DropdownComponent, DatepickerComponent, InputComponent],
   templateUrl: './side-filter.component.html',
-  styleUrl: './side-filter.component.css'
+  styleUrl: './side-filter.component.css',
 })
 export class SideFilterComponent {
-
+  attendance_status: AttendanceStatus[] = [
+    'in_late',
+    'in_ontime',
+    'onbreak',
+    'out_ontime',
+    'undertime',
+  ];
 }
