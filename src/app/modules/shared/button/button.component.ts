d4752f9ad1a6adb68f80d1fee6571adcfc27c7a9
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-02 18:54:09 +0800

    updates on shared button

--- src/app/modules/shared/button/button.component.ts
+++ src/app/modules/shared/button/button.component.ts
@@ -1,14 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+type ButtonSize = 'sm' | 'md' | 'lg';
+type ButtonType ='default'| 'primary' | 'success' | 'warning' | 'danger';
+
 @Component({
   selector: 'app-button',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
 export class ButtonComponent {
+  @Input() size: ButtonSize = 'sm';
+  @Input() type: ButtonType = 'default';
   @Output() btnclicked: EventEmitter<string> = new EventEmitter<string>();
 
   onClicked(): void {
