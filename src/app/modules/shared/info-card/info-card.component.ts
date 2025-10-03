057084526ec1f8da4218503e59fb1a369219d8d9
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-03 11:19:55 +0800

    update on info card and badge component

--- src/app/modules/shared/info-card/info-card.component.ts
+++ src/app/modules/shared/info-card/info-card.component.ts
@@ -1,15 +1,23 @@
-import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { BadgeComponent } from '../badge/badge.component';
+
+type CardSize = 'sm' | 'md' | 'lg';
+type CardType = 'default' | 'primary' | 'success' | 'warning' | 'danger';
 
 @Component({
   selector: 'app-info-card',
-  imports: [NgIf],
+  imports: [CommonModule, BadgeComponent],
   templateUrl: './info-card.component.html',
-  styleUrl: './info-card.component.css'
+  styleUrl: './info-card.component.css',
 })
 export class InfoCardComponent {
- @Input() title: string = ''
- @Input() description: string = ''
- @Input() infomini: string = 'n/a'
- @Input() displayTitle: boolean = false;
+  @Input() size: CardSize = 'sm';
+  @Input() type: CardType = 'primary';
+  @Input() badge: string = ''
+  @Input() subDetail: string = '' // detail on the right side of badge
+  @Input() title: string = '';
+  @Input() descriptions: string[] = [];
+  @Input() infomini: string = 'n/a';
+  @Input() displayTitle: boolean = false;
 }
