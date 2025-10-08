bb84248da12de26ced3c611527ab8d38b9d049b3
paulelyson <paulelyson.villaceran@gmail.com>
2025-10-08 19:22:14 +0800

    update on header

--- src/app/modules/shared/header/header.component.ts
+++ src/app/modules/shared/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
-  imports: []
+  imports: [MatIconModule, MatMenuModule],
 })
 export class HeaderComponent {
-
+  sidenav_opened: boolean = false;
 }
